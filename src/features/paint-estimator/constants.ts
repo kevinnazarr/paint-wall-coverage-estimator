ab6d5aa85c640195affcdf5c9cbdd923b6c2fd7d
kevinnazarr <kevinnazar76@gmail.com>
2026-09-08 10:04:07 +0700

    feat: constants

---
+++ src/features/paint-estimator/constants.ts
@@ -0,0 +1,7 @@
+export const DOOR_SQFT = 20
+export const WINDOW_SQFT = 15
+export const CAN_COVERAGE = { quart: 100, gallon: 400 } as const
+export const MAX_WALL_FT = 100
+export const MAX_CUTOUT_COUNT = 99
+export const WALL_COUNT_MIN = 1
+export const WALL_COUNT_MAX = 4
