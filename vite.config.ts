f17daf065a432449accf0e56fd177614fdf0ee84
kevinnazarr <kevinnazar76@gmail.com>
2026-09-08 10:05:57 +0700

    chore: vite

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  test: { environment: 'jsdom', globals: true, setupFiles: ['./test/setup.ts'], include: ['test/**/*.test.{ts,tsx}', 'src/**/*.test.{ts,tsx}'] },
+} as any)
